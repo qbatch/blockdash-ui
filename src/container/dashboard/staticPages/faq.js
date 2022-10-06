599d459ae4967c0d97f84eb638f17d48b0e5bcdc
Imran Ali <imran.ali@qbatch.com>
2022-10-06 12:24:32 +0500

    faq

--- src/container/dashboard/staticPages/faq.js
+++ src/container/dashboard/staticPages/faq.js
@@ -31,6 +31,7 @@ const Index = () => {
         "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
+    
     {
       title:
         "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
